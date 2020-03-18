de1bcd73fc5980319a20902be850546235e54582
christinerosquist <60344814+christinerosquist@users.noreply.github.com>
2020-03-18 14:48:13 +0100

    Update run-init.sh

--- sentry-tutorial/run-init.sh
+++ sentry-tutorial/run-init.sh
@@ -1,4 +1,4 @@
-ssh root@host01 "git --git-dir=/root/projects/DevOps/sentry-katacoda/sentry-tutorial/.git --work-tree=/root/projects/DevOps/sentry-katacoda/sentry-tutorial/.git pull"
+ssh root@host01 "git --git-dir=/sentry-katacoda/sentry-tutorial/sample-app.git --work-tree=/sentry-katacoda/sentry-tutorial/sample-app.git pull"
 ssh root@host01 "curl -k --silent --location https://rpm.nodesource.com/setup_12.x | sudo bash -"
 ssh root@host01 "yum -y install nodejs"
 ssh root@host01 "touch /etc/rhsm/ca/redhat-uep.pem"
