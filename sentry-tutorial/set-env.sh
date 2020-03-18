e41e7ab6194f07815c2556714401ef26ee32459d
christinerosquist <60344814+christinerosquist@users.noreply.github.com>
2020-03-18 15:39:26 +0100

    Delete set-env.sh

--- sentry-tutorial/set-env.sh
+++
@@ -1,4 +0,0 @@
-echo "Installing Node.js and NPM..."
-for i in {1..200}; do npm help >& /dev/null && break || (echo -n . ; sleep 2); done
-cd ~/projects/DevOps/sentry-katacoda/sentry-tutorial/sample-app
-clear
